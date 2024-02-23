0160807ec5367de9b226be9258d830aaef699bf5
devindiv <57625678+devindiv@users.noreply.github.com>
2024-02-23 16:43:49 +0530

    fixed post loading issue

--- components/RichTextComponent.tsx
+++ components/RichTextComponent.tsx
@@ -26,18 +26,17 @@ export const RichTextComponents = {
   },
   block: {
     h1: ({ children }: any) => (
-      <h1 className="text-5xl py-10 font-bold">{children}</h1>
+      <h1 className="text-5xl py-5 font-bold">{children}</h1>
     ),
     h2: ({ children }: any) => (
-      <h2 className="text-4xl py-10 font-bold">{children}</h2>
+      <h2 className="text-4xl py-4 font-bold">{children}</h2>
     ),
     h3: ({ children }: any) => (
-      <h3 className="text-3xl py-10 font-bold">{children}</h3>
+      <h3 className="text-3xl py-3 font-bold">{children}</h3>
     ),
     h4: ({ children }: any) => (
-      <h4 className="text-2xl py-10 font-bold">{children}</h4>
+      <h4 className="text-xl py-2 font-bold">{children}</h4>
     ),
-
     blockquote: ({ children }: any) => (
       <blockquote className="border-l-gray-500 border-l-4 pl-5 py-5 my-5">
         {children}
@@ -46,10 +45,10 @@ export const RichTextComponents = {
   },
   marks: {
     link: ({ children, value }: any) => {
-      const rel = !value.href.startsWith("/")
-        ? "noreferrer noopener"
-        : undefined;
-      return (
+      const rel = value.href?.startsWith("/")
+        ? undefined
+        : "noreferrer noopener";
+      return value.href ? (
         <Link
           href={value.href}
           rel={rel}
@@ -57,6 +56,10 @@ export const RichTextComponents = {
         >
           {children}
         </Link>
+      ) : (
+        <span className="underline decoration-slate-800 hover:decoration-orange-500">
+          {children}
+        </span>
       );
     },
   },
